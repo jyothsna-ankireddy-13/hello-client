b28568166f9cc951279f03c88ae0aa197a55d9a4
Vijay3582 <vijay8sap@gmail.com>
2021-10-21 15:33:49 +0530

    new service

--- hello_client/hello-client/src/pages/Services.js
+++ hello_client/hello-client/src/pages/Services.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import {Card, CardContent,Typography, CardActions, IconButton} from '@material-ui/core';
+import * as FaIcons from 'react-icons/fa';
+import './Card.css';
+import CustomizedDialogs from './Servicepopup';
+import NewService from './NewService';
 const Services = () => {
     return (
         <div className="services">
-            <h1>Services</h1>
+            <div>
+                <Card className="card-container" >
+                    <CardContent className="card-content">
+                        <Typography gutterBottom  variant="h5">
+                            Create New Service
+                        </Typography>
+                    </CardContent>
+                    <CardActions className="cardActions">
+                        <IconButton>
+                            <FaIcons.FaPlus/>
+                        </IconButton>
+                        <CustomizedDialogs title="Create New Service">
+                            <NewService/>
+                        </CustomizedDialogs>    
+                    </CardActions>
+                    
+                </Card>
+            </div>
         </div>
     )
 }
