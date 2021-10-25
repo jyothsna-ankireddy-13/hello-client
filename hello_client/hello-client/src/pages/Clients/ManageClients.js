7c7b11cce737e9808974460ff5c93c009e8c2ca7
Vijay3582 <vijay8sap@gmail.com>
2021-10-25 18:00:14 +0530

    tabs

--- hello_client/hello-client/src/pages/Clients/ManageClients.js
+++ hello_client/hello-client/src/pages/Clients/ManageClients.js
@@ -1,28 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react';
 import * as FaIcons from 'react-icons/fa';
 import {Card, CardContent,Typography, CardActions, Button} from '@material-ui/core';
 import './ClientCard.css';
 import {Link} from 'react-router-dom';
 const ManageService = () => {
+
+    const [toggleState,setToggleState] = useState(1);
+    const toggleTab = (index) =>{
+        setToggleState(index);
+    }
+
     return (
         <div className="clients"> 
-        <div>
-          <div className="clientname-heading">
-              <Typography variant="h3">Client Name</Typography>
-          </div>
-          <div className="client-main">
-          <div className="client-main">
-            <Typography variant="h4">Client Full Name</Typography>
-            <Typography>Client Description.The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. </Typography>
-            
-          </div>
-          <div className="client-details">
-              <div>
-                <Typography>Contact Person:XXXXXX</Typography>
-                <Typography>Phone:4750435496</Typography>
-                <Typography>Email:contact@company.com</Typography>
-                <Typography>Website:www.client.com</Typography>
-              </div>
+            <div>
+            <div className="clientname-heading">
+                <Typography variant="h3">Client Name</Typography>
+            </div>
+            <div className="client-main">
+            <div className="client-main">
+                <Typography variant="h4">Client Full Name</Typography>
+                <Typography>Client Description.The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. </Typography>
+                
+            </div>
+            <div className="client-details">
+                <div>
+                    <Typography>Contact Person:XXXXXX</Typography>
+                    <Typography>Phone:4750435496</Typography>
+                    <Typography>Email:contact@company.com</Typography>
+                    <Typography>Website:www.client.com</Typography>
+                </div>
                 <div>
                     <Typography>Address:</Typography>
                     <Typography>XXXXXXXXXXXXX</Typography>
@@ -37,47 +43,62 @@ const ManageService = () => {
             </div>
         <div>
             <Link to="/addexpense" className="remind-all">
-                <FaIcons.FaPlus/> Add Expense
+                <FaIcons.FaPlus/> Add Payment
             </Link>
         </div>
-        <div className="cards-wrap">
-            <div className="cards">
-                <Card className="card-container" >
-                    <CardContent className="card-content">
-                        <Typography gutterBottom  variant="h5">
-                            Add Service
-                        </Typography>
-                    </CardContent>{/*
-                    <CardActions className="cardActions">
-                        <CustomizedDialogs title="Add Service">
-                            <NewService/>
-                        </CustomizedDialogs>    
-                    </CardActions>*/}
-                    
-                </Card>
-                <Card className="card-container" id="service-card2">
-                <Typography gutterBottom  variant="h5">
-                            Service Name
-                </Typography>
+        <div className="container">
+            <div className="bloc-tabs">
+                <div className= { toggleState === 1 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(1)}>Clients</div>
+                <div className={ toggleState === 2 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(2)}>Expenses</div>
+            </div>
+            <div className="content-tabs">
+                < div className={ toggleState === 1 ? "content active-content" : "content"}>
+                    <div className="cards-wrap">
+                        <div className="cards">
+                            <Card className="card-container" >
+                                <CardContent className="card-content">
+                                    <Typography gutterBottom  variant="h5">
+                                        Add Service
+                                    </Typography>
+                                </CardContent>{/*
+                                <CardActions className="cardActions">
+                                    <CustomizedDialogs title="Add Service">
+                                        <NewService/>
+                                    </CustomizedDialogs>    
+                                </CardActions>*/}
+                                
+                            </Card>
+                            <Card className="card-container" id="service-card2">
+                                <Typography gutterBottom  variant="h5">
+                                            Service Name
+                                </Typography>
                         
-                <div className="card-display">
-                    <CardContent className="card-content">
-                       
-                        <Typography>Service Category Name</Typography>
-                        <Typography>Billing Cycle:dd mm yy(monthly)</Typography>
-                        <Typography>Revenue:XXX</Typography>
-                        <Typography>Overdue Amount:XXXX</Typography>
-                    </CardContent>
-                    </div>
-                    <CardActions className="cardActions"> 
-                        <Button className="remind-btn">Remind</Button>
-                        <Button className="remind-all">Manage</Button>    
-                    </CardActions>
+                                <div className="card-display">
+                                    <CardContent className="card-content">
+                                    
+                                        <Typography>Service Category Name</Typography>
+                                        <Typography>Billing Cycle:dd mm yy(monthly)</Typography>
+                                        <Typography>Revenue:XXX</Typography>
+                                        <Typography>Overdue Amount:XXXX</Typography>
+                                    </CardContent>
+                                </div>
+                                <div >
+                                    <CardActions className="cardActions"> 
+                                        <Button className="remind-btn">Remind</Button>
+                                        <Button className="remind-all">Manage</Button>    
+                                    </CardActions>
+                                </div>
                     
-                </Card>
+                            </Card>
+                                </div>
+                            </div>
+                        </div>
+                    <div className={ toggleState === 2 ? "content active-content" : "content"}>
+                        <p>Expenses Tab</p>
+                    </div>
+                </div>
+               </div>
             </div>
-            </div>
-        </div>
         </div>
     )
 }
--- hello_client/hello-client/src/pages/Services/ManageService.js
+++ hello_client/hello-client/src/pages/Services/ManageService.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react';
 import * as FaIcons from 'react-icons/fa';
 import {Card, CardContent,Typography, CardActions, Button} from '@material-ui/core';
 import './Card.css';
@@ -6,6 +6,12 @@ import CustomizedDialogs from './Servicepopup';
 import NewService from './NewService';
 import {Link} from 'react-router-dom';
 const ManageService = () => {
+
+    const [toggleState,setToggleState] = useState(1);
+    const toggleTab = (index) =>{
+        setToggleState(index);
+    }
+
     return (
         <div className="manage-service"> 
           <div className="servicename-heading">
@@ -46,48 +52,62 @@ const ManageService = () => {
                 <FaIcons.FaPlus/> Add Expense
             </Link>
         </div>
-        <div className="cards-wrap">
-            <div className="cards">
-                <Card className="card-container" >
-                    <CardContent className="card-content">
-                        <Typography gutterBottom  variant="h5">
-                            Add Client
-                        </Typography>
-                    </CardContent>
-                    <CardActions className="cardActions">
-                        <CustomizedDialogs title="Create New Service">
-                            <NewService/>
-                        </CustomizedDialogs>    
-                    </CardActions>
-                    
-                </Card>
-                <Card className="card-container" id="service-card2">
-                <Typography gutterBottom  variant="h5">
-                            Client Name
-                </Typography>
-                        
-                <div className="card-display">
-                    <CardContent className="card-content">
-                       
-                        <Typography>Services:X</Typography>
-                        <Typography>Payment overdue:X</Typography>
-                        <Typography>Revenue:XXX</Typography>
-                    </CardContent>
-                    <CardContent className="card-content">
-                        <Typography>Renewals in next 30d:X</Typography>
-                        <Typography>Overdue Amount:XXXX</Typography>
-                        <Typography>Cost:XXXX</Typography>
-                    </CardContent>
-                    </div>
-                    <CardActions className="cardActions"> 
-                        <Button className="remind-btn">Remind</Button>
-                        <Button className="remind-all">Manage</Button>    
-                    </CardActions>
-                    
-                </Card>
+        <div className="container">
+            <div className="bloc-tabs">
+                <div className= { toggleState === 1 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(1)}>Clients</div>
+                <div className={ toggleState === 2 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(2)}>Expenses</div>
             </div>
+            <div className="content-tabs">
+                <div className={ toggleState === 1 ? "content active-content" : "content"}>
+                    <div className="cards-wrap">
+                        <div className="cards">
+                            <Card className="card-container" >
+                                <CardContent className="card-content">
+                                    <Typography gutterBottom  variant="h5">
+                                        Add Client
+                                    </Typography>
+                                </CardContent>
+                                <CardActions className="cardActions">
+                                    <CustomizedDialogs title="Create New Service">
+                                        <NewService/>
+                                    </CustomizedDialogs>    
+                                </CardActions>
+                                
+                            </Card>
+                            <Card className="card-container" id="service-card2">
+                            <Typography gutterBottom  variant="h5">
+                                        Client Name
+                            </Typography>
+                                    
+                            <div className="card-display">
+                                <CardContent className="card-content">
+                                
+                                    <Typography>Services:X</Typography>
+                                    <Typography>Payment overdue:X</Typography>
+                                    <Typography>Revenue:XXX</Typography>
+                                </CardContent>
+                                <CardContent className="card-content">
+                                    <Typography>Renewals in next 30d:X</Typography>
+                                    <Typography>Overdue Amount:XXXX</Typography>
+                                    <Typography>Cost:XXXX</Typography>
+                                </CardContent>
+                            </div>
+                                <CardActions className="cardActions"> 
+                                    <Button className="remind-btn">Remind</Button>
+                                    <Button className="remind-all">Manage</Button>    
+                                </CardActions>
+                                
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+                <div className={ toggleState === 2 ? "content active-content" : "content"}>
+                    <p>Expenses Tab</p>
+                </div>
+                
             </div>
         </div>
+    </div>
     )
 }
 
